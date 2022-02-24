c64d789b82907841c0de69e51d0e14aefb4dfc61
DannyAnriquez <anriquezda@gmail.com>
2022-02-23 21:05:09 -0300

    finish

--- src/components/DolarValue.jsx
+++ src/components/DolarValue.jsx
@@ -20,6 +20,8 @@ const tipoArray = Object.keys(responseJSON)
 
 setDolar(dolarArray)
 setTipo(tipoArray)
+
+console.log(dolar)
 }
 
 useEffect(() => { 
@@ -38,19 +40,46 @@ useEffect(() => {
         </tr>
       </MDBTableHead>
       <MDBTableBody>
-        { 
-        
-        dolar.map((data, index) => 
-        <tr key={index} className='table-dark'>
-        <th scope='row'>Oficial</th>
-       <td>${data}.-</td>
-       
+      <tr className='table-dark'>
+        <th scope='row'>{tipo[0]} - Cotización Banco Nación</th>
+       <td>${dolar[0]}.-</td>
       </tr>
-       )}
+      <tr className='table-dark'>
+        <th scope='row'>{tipo[1]} - Cotización con impuesto PAIS + Imp. GANANCIAS.</th>
+       <td>${dolar[1]}.-</td>
+      </tr>
+      <tr className='table-dark'>
+        <th scope='row'>{tipo[2]} - Dólar Bolsa (calculado con el bono AL30).</th>
+       <td>${dolar[2]}.-</td>
+      </tr>
+      <tr className='table-dark'>
+        <th scope='row'>{tipo[3]} - Dólar Bolsa (calculado con el bono GD30).</th>
+       <td>${dolar[3]}.-</td>
+      </tr>
+      <tr className='table-dark'>
+        <th scope='row'>{tipo[4]} - Dólar Contado con Liquidación (calculado con el bono AL30).</th>
+       <td>${dolar[4]}.-</td>
+      </tr>
+      <tr className='table-dark'>
+        <th scope='row'>{tipo[5]} - Dólar Contado con Liquidación (calculado con el bono GD30).</th>
+       <td>${dolar[5]}.-</td>
+      </tr>
+      <tr className='table-dark'>
+        <th scope='row'>{tipo[6]} - Dólar Contado con Bitcoin.</th>
+       <td>${dolar[6]}.-</td>
+      </tr>
+      <tr className='table-dark'>
+        <th scope='row'>{tipo[7]} - Dólar Informal</th>
+       <td>${dolar[7]}.-</td>
+      </tr>
 
         
       </MDBTableBody>
     </MDBTable>
+
+    <div>
+      <p>Ultima actualizacion {dolar[8]} </p>
+    </div>
 
   </div>
   )};
